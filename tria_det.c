820a2165c25730607898a835ad671675875abfb9
ViktorooReps <opd.pacc@gmail.com>
2021-11-16 11:48:22 +0300

    Revert "temporary pragma removal"
    This reverts commit e6b5fe4109b2b1bc5674cf9062244f09380e40d9.

--- tria_det.c
+++ tria_det.c
@@ -26,7 +26,7 @@ det(double **matrix, size_t len, size_t threads)
     double det = 1.0;
     
     int r_idx, c_idx;
-
+#pragma omp parallel for private(r_idx, c_idx) shared(matrix, det) num_threads(threads)
     for (r_idx = 0; r_idx < len; ++r_idx) {
         for (c_idx = 0; c_idx < r_idx; ++c_idx) {
             double elem = matrix[r_idx][c_idx];
