608c768e990a1da61ac68729fa162a577b26f797
ViktorooReps <opd.pacc@gmail.com>
2021-11-16 13:29:12 +0300

    final results

--- create_graphs.py
+++ create_graphs.py
@@ -6,13 +6,29 @@ import dataframe_image as dfi
 
 if __name__ == '__main__':
     data = pd.read_csv('data.csv', sep='\t')
+    data = data[data['n'] > 2]
+    small_data = data[data['n'] < 2 ** 6]
+    big_data = data[data['n'] > 2 ** 7]
 
     palette = sns.color_palette("flare", 8)
     sns.set()
+
     ax = sns.lineplot(data=data, x='n', y='time', hue='threads', palette=palette)
     ax.set_title('Execution time in seconds with nxn matrix')
     ax.set_xscale('log', basex=2)
     plt.savefig('lineplot.png')
+    plt.show()
+
+    ax = sns.lineplot(data=small_data, x='n', y='time', hue='threads', palette=palette)
+    ax.set_title('Execution time in seconds with nxn matrix on smaller n')
+    ax.set_xscale('log', basex=2)
+    plt.savefig('lineplot_small.png')
+    plt.show()
+
+    ax = sns.lineplot(data=big_data, x='n', y='time', hue='threads', palette=palette)
+    ax.set_title('Execution time in seconds with nxn matrix on larger n')
+    ax.set_xscale('log', basex=2)
+    plt.savefig('lineplot_big.png')
     plt.show()
 
     data = data.pivot("n", "threads", "time")
