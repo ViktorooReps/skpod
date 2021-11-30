6fe31b411de4b25a76611da6b906a5c419cf91bb
ViktorooReps <opd.pacc@gmail.com>
2021-11-30 16:05:12 +0300

    removed pathlib

--- run_bluegene.py
+++ run_bluegene.py
@@ -1,6 +1,5 @@
 import logging
 import sys
-from pathlib import Path
 
 from utils import create_parser, xlc_compile, schedule, Machine, wait, collect_results, save_results
 
@@ -13,13 +12,13 @@ if __name__ == '__main__':
     args = arg_parser.parse_args()
 
     compile_args = ['-qsmp=omp', '-o', args.exec_file]
-    xlc_compile(Path(args.src_file), compile_args)
+    xlc_compile(args.src_file, compile_args)
 
     results = []
     for n_proc in args.processes:
         job_name = args.experiment + '-' + str(n_proc) + ("p" if args.mpi else "t")
 
-        schedule(Machine.BLUEGENE, n_proc, exec_file=Path(args.exec_file), res_filename=job_name,
+        schedule(Machine.BLUEGENE, n_proc, exec_file=args.exec_file, res_filename=job_name,
                  use_threads=not args.mpi)
         wait(job_name)
 
