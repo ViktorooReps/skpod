fa58bf79884b77933b74c1351d151fa05d15f2e4
ViktorooReps <opd.pacc@gmail.com>
2021-12-01 20:03:59 +0300

    master-slave mpi

--- mpi/src/tria_det.c
+++ mpi/src/tria_det.c
@@ -8,6 +8,7 @@
 #define N_MATRIX_LENS 10
 #define SEED 42
 #define MAX_DET_VALUE 10000.0
+#define MASTER_RANK 0
 
 void
 mult_row_from_idx(double *row, double num, size_t row_len, size_t from_idx)
@@ -25,117 +26,95 @@ add_row_from_idx(double *row_dest, double *row_to_add, size_t row_len, size_t fr
     }
 }
 
-int
-mpi__in_thread_group(int rank, int mpi__thread_group, int mpi__thread_group_len)
+double
+det(double **matrix, size_t len)
 {
-    int mpi__thread_group_start = mpi__thread_group * mpi__thread_group_len;
-    int mpi__thread_group_end = mpi__thread_group_start + mpi__thread_group_len;
+    double det = 1.0;
+    for (int diag_idx = 0; diag_idx < len; ++diag_idx) {
+        // reset diagonal element to 1.0
+        double diag_elem = matrix[diag_idx][diag_idx];
+        mult_row_from_idx(matrix[diag_idx], 1.0 / diag_elem, len, diag_idx);
+        det *= diag_elem;
 
-    return ((rank >= mpi__thread_group_start) && (rank < mpi__thread_group_end));
-}
-
-void
-mpi__mult_row_from_idx(double *row, double num, size_t row_len, int from_idx, int mpi__thread_group, int mpi__thread_group_len, int rank)
-{
-    int proc_len = row_len - from_idx;
-    int mpi__in_group_rank = rank - mpi__thread_group * mpi__thread_group_len;
-    int mpi__new_group_len = mpi__thread_group_len % proc_len;
-
-    if (mpi__in_thread_group(mpi__in_group_rank, 0, mpi__new_group_len)) {
-        int mpi__elems_per_thread = proc_len / mpi__new_group_len;
-        int mpi__start_elem = mpi__in_group_rank * mpi__elems_per_thread;
-        int mpi__end_elem = mpi__start_elem + mpi__elems_per_thread;
-
-        if (mpi__in_group_rank == mpi__new_group_len - 1) {
-            mpi__end_elem = row_len - from_idx;
+        // reset elements under diagonal to 0
+        int col_idx = diag_idx;
+        for (int row_idx = diag_idx + 1; row_idx < len; ++row_idx) {
+            double elem = matrix[row_idx][col_idx];
+            mult_row_from_idx(matrix[row_idx], -1.0 / elem, len, col_idx);
+            det *= -1.0 * elem;
+            add_row_from_idx(matrix[row_idx], matrix[diag_idx], len, col_idx);
         }
-
-        for (int elem_idx = mpi__start_elem; elem_idx < mpi__end_elem; ++elem_idx) {
-            row[from_idx + elem_idx] *= num;
-        }
-
-        MPI_Bcast(row + from_idx + mpi__start_elem, mpi__end_elem - mpi__start_elem, MPI_DOUBLE, rank, MPI_COMM_WORLD);
     }
+    return det
 }
 
-void
-mpi__add_row_from_idx(double *row_dest, double *row_to_add, size_t row_len, int from_idx, int mpi__thread_group, int mpi__thread_group_len, int rank)
-{
-    int proc_len = row_len - from_idx;
-    int mpi__in_group_rank = rank - mpi__thread_group * mpi__thread_group_len;
-    int mpi__new_group_len = mpi__thread_group_len % proc_len;
-
-    if (mpi__in_thread_group(mpi__in_group_rank, 0, mpi__new_group_len)) {
-        int mpi__elems_per_thread = proc_len / mpi__new_group_len;
-        int mpi__start_elem = mpi__in_group_rank * mpi__elems_per_thread;
-        int mpi__end_elem = mpi__start_elem + mpi__elems_per_thread;
-
-        if (mpi__in_group_rank == mpi__new_group_len - 1) {
-            mpi__end_elem = row_len - from_idx;
-        }
-
-        for (int elem_idx = mpi__start_elem; elem_idx < mpi__end_elem; ++elem_idx) {
-            row_dest[from_idx + elem_idx] += row_to_add[from_idx + elem_idx];
-        }
-
-        MPI_Bcast(row_dest + from_idx + mpi__start_elem, mpi__end_elem - mpi__start_elem, MPI_DOUBLE, rank, MPI_COMM_WORLD);
-    }
-}
 
 double
 mpi__det(double **matrix, size_t len, size_t threads, int rank)
 {
-    double det = 1.0;
+    double det = 1.0, res = 1.0;
 
+    double *diag_row = malloc(sizeof(double) * len);
     for (int diag_idx = 0; diag_idx < len; ++diag_idx) {
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // reset to 1.0 diagonal element
-        double diag = matrix[diag_idx][diag_idx];
-        mpi__mult_row_from_idx(matrix[diag_idx], 1.0 / diag, len, diag_idx, 0, threads, rank);
         if (!rank) {
-            det *= diag;
+            diag_row = matrix[diag_idx];
+            double diag_elem = diag_row[diag_idx];
+            mult_row_from_idx(diag_row, 1.0 / diag_elem, len, diag_idx);
+            det *= diag_elem;
         }
 
-        int mpi__start_row, mpi__end_row, mpi__threads_per_row, mpi__rows_per_thread;
-        if (threads > len) {
-            mpi__rows_per_thread = 1;
-            mpi__threads_per_row = threads / len;
-            mpi__start_row = rank / mpi__threads_per_row;
-            mpi__end_row = mpi__start_row + 1;
+        MPI_Bcast(diag_row, len, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        int slave_threads = threads - 1;
+
+        if (!rank) {
+            int total_requests = (len - diag_idx - 1) * 2;
+            MPI_Request *requests = malloc(sizeof(MPI_Request) * total_requests);
+
+            int curr_req_idx = 0, dest;
+            // send data to slave processes
+            for (int row_idx = diag_idx + 1; row_idx < len; ++row_idx) {
+                dest = row_idx % slave_threads + 1;
+                MPI_Isend(matrix[row_idx], len, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD, requests + curr_req_idx);
+                curr_req_idx += 1;
+            }
+            // receive data from slave processes
+            for (int row_idx = diag_idx + 1; row_idx < len; ++row_idx) {
+                dest = row_idx % slave_threads + 1;
+                MPI_Irecv(matrix[row_idx], len, MPI_DOUBLE, dest, 0, MPI_COMM_WORLD, requests + curr_req_idx);
+                curr_req_idx += 1;
+            }
+            // wait for requests completion
+            for (int req_idx = 0; req_idx < total_requests; ++req_idx) {
+                MPI_Wait(requests + req_idx, MPI_STATUS_IGNORE);
+            }
+
+            free(requests);
         } else {
-            mpi__rows_per_thread = len / threads;
-            mpi__threads_per_row = 1;
-            mpi__start_row = rank * mpi__rows_per_thread;
-            mpi__end_row = mpi__start_row + mpi__rows_per_thread;
+            int col_idx = diag_idx;
+            int assigned_row = rank;
+            double *compute_row = malloc(sizeof(double) * len);
+            while (assigned_row < len) {
+                // receive data from master process
+                MPI_Recv(compute_row, len, MPI_DOUBLE, MASTER_RANK, 0, MPI_COMM_WORLD);
 
-            if (rank == threads - 1) {
-                mpi__end_row = len;
-            }
-        }
-
-        int mpi__thread_group, mpi__in_group_rank, mpi__thread_group_len;
-        mpi__thread_group_len = mpi__threads_per_row;
-        mpi__thread_group = rank / mpi__threads_per_row;
-        mpi__in_group_rank = rank - mpi__thread_group * mpi__threads_per_row;
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // reset to zero elements under diagonal
-        int col_idx = diag_idx;
-        for (int row_idx = mpi__start_row; row_idx < mpi__end_row; ++row_idx) {
-            double elem = matrix[row_idx][col_idx];
-            mpi__mult_row_from_idx(matrix[row_idx], -1.0 / elem, len, col_idx, mpi__thread_group, mpi__thread_group_len, rank);
-            if (!mpi__in_group_rank) {
+                // reset (assigned_row, col_idx) element to zero
+                double elem = compute_row[col_idx];
+                mult_row_from_idx(compute_row, -1.0 / elem, len, col_idx);
                 det *= -1.0 * elem;
-            }
+                add_row_from_idx(compute_row, diag_row, len, col_idx);
 
-            mpi__add_row_from_idx(matrix[row_idx], matrix[diag_idx], len, col_idx, mpi__thread_group, mpi__thread_group_len, rank);
+                // send modified data back to master
+                MPI_Send(compute_row, len, MPI_DOUBLE, MASTER_RANK, 0, MPI_COMM_WORLD);
+
+                assigned_row += slave_threads;
+            }
+            free(compute_row);
         }
+
     }
 
-    double res = 1.0;
-    MPI_Barrier(MPI_COMM_WORLD);
+    free(diag_row);
+
     MPI_Reduce(&det, &res, 1, MPI_DOUBLE, MPI_PROD, 0, MPI_COMM_WORLD);
 
     return res;
@@ -197,8 +176,16 @@ main(int argc, char **argv)
             }
 
             MPI_Barrier(MPI_COMM_WORLD);
+
             timer_mpi = MPI_Wtime();
-            double d = mpi__det(matrix, n[i], threads, rank);
+
+            double d
+            if (threads == 1) {
+                d = det(matrix, n[i]);
+            } else {
+                d = mpi__det(matrix, n[i], threads, rank);
+            }
+
             avg_time += MPI_Wtime() - timer_mpi;
         }
         avg_time /= (double)N_RUNS;
