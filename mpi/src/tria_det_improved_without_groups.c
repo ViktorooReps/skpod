2b5c2cb865735f86dd248ac299693b8fa235faf6
ViktorooReps <opd.pacc@gmail.com>
2021-12-16 14:21:26 +0300

    src for bluegene

--- mpi/src/tria_det_improved_without_groups.c
+++ mpi/src/tria_det_improved_without_groups.c
@@ -103,100 +103,82 @@ det(double *matrix, size_t len)
 double
 mpi__det(double *matrix, size_t len, size_t threads, int rank)
 {
-    MPI_Group world_group;
-    MPI_Comm_group(MPI_COMM_WORLD, &world_group);
-
     double res = 1.0;
 
     int working_threads = ((threads > len)? len : threads);
-    int *working_ranks = malloc(sizeof(int) * working_threads);
+    int working_rank = rank;
 
-    for (int worker_idx = 0; worker_idx < working_threads; ++worker_idx) {
-        working_ranks[worker_idx] = worker_idx;
+    // distribute rows among working processes
+    int *displacements = malloc(sizeof(int) * len);
+    int *send_counts = malloc(sizeof(int) * len);
+
+    for (int row_idx = 0; row_idx < len; ++row_idx) {
+        send_counts[row_idx] = len;
+        displacements[row_idx] = row_idx % working_threads;
     }
 
-    MPI_Group working_group;
-    MPI_Group_incl(world_group, working_threads, working_ranks, &working_group);
-
-    MPI_Comm working_comm;
-    MPI_Comm_create_group(MPI_COMM_WORLD, working_group, 0, &working_comm);
-
-    if (working_comm != MPI_COMM_NULL) {
-        int working_rank;
-        MPI_Comm_rank(working_comm, &working_rank);
-
-        // distribute rows among working processes
-        int *displacements = malloc(sizeof(int) * len);
-        int *send_counts = malloc(sizeof(int) * len);
-
-        for (int row_idx = 0; row_idx < len; ++row_idx) {
-            send_counts[row_idx] = len;
-            displacements[row_idx] = row_idx % working_threads;
-        }
-
-        int assigned_rows = len / working_threads + (rank < len % working_threads);
-
-        double *compute_rows = malloc(sizeof(double) * len * assigned_rows);
-        double *diag_row = malloc(sizeof(double) * len);
-
-        MPI_Scatterv(matrix, send_counts, displacements,
-                     MPI_DOUBLE, compute_rows, assigned_rows,
-                     MPI_DOUBLE, MASTER_RANK, working_comm);
-
-        free(displacements);
-        free(send_counts);
-
-        // everything is ready for determinant computation
-        double det = 1.0;
-        double *non_zero_diag_row;
-        int diag_assigned_working_rank, curr_row_len, non_zeros;
-
-        for (int diag_idx = 0; diag_idx < len; ++diag_idx) {
-            diag_assigned_working_rank = diag_idx % working_threads;
-            curr_row_len = len - diag_idx;
-            non_zeros = diag_idx;
-            non_zero_diag_row = diag_row + non_zeros;
-
-            if (working_rank == diag_assigned_working_rank) {
-                int diag_idx_in_compute_rows = diag_idx / working_threads;
-                int diag_row_offset = diag_idx_in_compute_rows * len;
-                double *non_zero_compute_row = compute_rows + diag_row_offset + non_zeros;
-
-                // reset diagonal element to 1.0
-                double diag_elem = *non_zero_compute_row;
-                mult_row(non_zero_compute_row, 1.0 / diag_elem, curr_row_len);
-                det *= diag_elem;
-
-                // copy computed row to buffer
-                memcpy(non_zero_diag_row, non_zero_compute_row, curr_row_len * sizeof(double));
-            }
-
-            // send new diagonal row to all processes
-            MPI_Bcast(diag_row + non_zeros, curr_row_len, MPI_DOUBLE, diag_assigned_working_rank, working_comm);
-
-            // reset elements under diagonal to 0 for all assigned rows
-            for (int row_idx = 0; row_idx < assigned_rows; ++row_idx) {
-                int row_offset = row_idx * len;
-                double *non_zero_row = compute_rows + row_offset + non_zeros;
-
-                double elem = *non_zero_row;
-                mult_row(non_zero_row, -1.0 / elem, curr_row_len);
-                det *= -1.0 * elem;
-
-                add_row(non_zero_row, non_zero_diag_row, curr_row_len);
-            }
-        }
-
-        MPI_Reduce(&det, &res, 1, MPI_DOUBLE, MPI_PROD, MASTER_RANK, working_comm);
-
-        free(diag_row);
-        free(compute_rows);
+    int assigned_rows;
+    if (rank < working_threads) {
+        assigned_rows = len / working_threads + (rank < len % working_threads);
+    } else {
+        assigned_rows = 0;
     }
 
-    MPI_Group_free(&world_group);
-    MPI_Group_free(&working_group);
+    double *compute_rows = malloc(sizeof(double) * len * assigned_rows);
+    double *diag_row = malloc(sizeof(double) * len);
 
-    free(working_ranks);
+    MPI_Scatterv(matrix, send_counts, displacements,
+                 MPI_DOUBLE, compute_rows, assigned_rows,
+                 MPI_DOUBLE, MASTER_RANK, MPI_COMM_WORLD);
+
+    free(displacements);
+    free(send_counts);
+
+    // everything is ready for determinant computation
+    double det = 1.0;
+    double *non_zero_diag_row;
+    int diag_assigned_working_rank, curr_row_len, non_zeros;
+
+    for (int diag_idx = 0; diag_idx < len; ++diag_idx) {
+        diag_assigned_working_rank = diag_idx % working_threads;
+        curr_row_len = len - diag_idx;
+        non_zeros = diag_idx;
+        non_zero_diag_row = diag_row + non_zeros;
+
+        if (working_rank == diag_assigned_working_rank) {
+            int diag_idx_in_compute_rows = diag_idx / working_threads;
+            int diag_row_offset = diag_idx_in_compute_rows * len;
+            double *non_zero_compute_row = compute_rows + diag_row_offset + non_zeros;
+
+            // reset diagonal element to 1.0
+            double diag_elem = *non_zero_compute_row;
+            mult_row(non_zero_compute_row, 1.0 / diag_elem, curr_row_len);
+            det *= diag_elem;
+
+            // copy computed row to buffer
+            memcpy(non_zero_diag_row, non_zero_compute_row, curr_row_len * sizeof(double));
+        }
+
+        // send new diagonal row to all processes
+        MPI_Bcast(diag_row + non_zeros, curr_row_len, MPI_DOUBLE, diag_assigned_working_rank, MPI_COMM_WORLD);
+
+        // reset elements under diagonal to 0 for all assigned rows
+        for (int row_idx = 0; row_idx < assigned_rows; ++row_idx) {
+            int row_offset = row_idx * len;
+            double *non_zero_row = compute_rows + row_offset + non_zeros;
+
+            double elem = *non_zero_row;
+            mult_row(non_zero_row, -1.0 / elem, curr_row_len);
+            det *= -1.0 * elem;
+
+            add_row(non_zero_row, non_zero_diag_row, curr_row_len);
+        }
+    }
+
+    MPI_Reduce(&det, &res, 1, MPI_DOUBLE, MPI_PROD, MASTER_RANK, working_comm);
+
+    free(diag_row);
+    free(compute_rows);
 
     return res;
 }
