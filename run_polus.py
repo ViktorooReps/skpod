cf6c0952bb04288d6f8f4cee904423d0170f8d74
ViktorooReps <opd.pacc@gmail.com>
2021-12-18 15:23:53 +0300

    compile fix

--- run_polus.py
+++ run_polus.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     args = arg_parser.parse_args()
 
     if args.mpi:
-        compile_args = [args.exec_file]
+        compile_args = ['-o', args.exec_file]
         mpixlc_compile(args.src_file, compile_args)
     else:
         compile_args = ['-qsmp=omp', '-o', args.exec_file]
