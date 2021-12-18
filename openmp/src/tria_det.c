63308da9982096bae6fd49b73f3058501d77617a
ViktorooReps <opd.pacc@gmail.com>
2021-12-18 11:51:57 +0300

    ???

--- openmp/src/tria_det.c
+++ openmp/src/tria_det.c
@@ -111,28 +111,25 @@ double
 omp__det(double *matrix, size_t len, int threads)
 {
     int diag_idx, row_idx;
-    double *matrix_copy = alloc_matrix(len), *non_zero_diag_row, *non_zero_row;
+    double *matrix_copy = alloc_matrix(len);
     memcpy(matrix_copy, matrix, len * len * sizeof(double));
 
     double det = 1.0, elem;
-#pragma omp parallel for private(diag_idx, row_idx, elem, len) shared(matrix_copy, non_zero_row, non_zero_diag_row, det) num_threads(threads)
+#pragma omp parallel for private(diag_idx, row_idx, elem, len) shared(matrix_copy, det) num_threads(threads)
     for (diag_idx = 0; diag_idx < len; ++diag_idx) {
-        non_zero_diag_row = matrix_copy + len * diag_idx + diag_idx;
-
         // reset diagonal element to 1.0
-        elem = *non_zero_diag_row;
-        mult_row(non_zero_diag_row, 1.0 / elem, len - diag_idx);
+        elem = matrix_copy[len * diag_idx + diag_idx];
+        mult_row(matrix_copy + len * diag_idx + diag_idx, 1.0 / elem, len - diag_idx);
         det *= elem;
 
         // reset elements under diagonal to 0
         for (row_idx = diag_idx + 1; row_idx < len; ++row_idx) {
-            non_zero_row = matrix_copy + row_idx * len + diag_idx;
-
-            elem = *non_zero_row;
-            mult_row(non_zero_row, -1.0 / elem, len - diag_idx);
+            elem = matrix_copy[row_idx * len + diag_idx];
+            mult_row(matrix_copy + row_idx * len + diag_idx, -1.0 / elem, len - diag_idx);
             det *= -1.0 * elem;
 
-            add_row(non_zero_row, non_zero_diag_row, len - diag_idx);
+            add_row(matrix_copy + row_idx * len + diag_idx, matrix_copy + len * diag_idx + diag_idx,
+                    len - diag_idx);
         }
     }
 
